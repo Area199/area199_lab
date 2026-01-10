9bdbc875942dac113200085c330c4b4206b52ec5
Area199 <info@area199.com>
2026-01-10 17:24:28 +0100

    app.py

--- app.py
+++ app.py
@@ -121,8 +121,8 @@ def find_exercise_images(name_query, db_exercises):
     if not db_exercises or not name_query: return []
     BASE_URL = "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/"
     db_names = [x['name'] for x in db_exercises]
-    match = process.extractOne(name_query, db_names, scorer=fuzz.token_set_ratio)
-    if match and match[1] > 60:
+    match = process.extractOne(name_query, db_names, scorer=fuzz.fuzz.token_sort_ratio)
+    if match and match[1] > 85:
         for ex in db_exercises:
             if ex['name'] == match[0]:
                 return [BASE_URL + img for img in ex.get('images', [])]
@@ -373,3 +373,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
